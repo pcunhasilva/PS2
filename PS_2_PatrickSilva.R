d9aab12ba7fcd4274d315e88acf528149b5e3148
pcunhasilva <patricksilva27@gmail.com>
2017-02-03 23:33:45 -0600

    Add save_benford_csv
    Add save_benfords_csv;
    Add a safety check in benford;
    Add commentaries in benford.

--- PS_2_PatrickSilva.R
+++ PS_2_PatrickSilva.R
@@ -22,27 +22,32 @@ data_matrix <- matrix(round(rexp(64)*1000), nrow = 8, ncol = 8)
 # first digit and the selected statistics.
 
 benford <- function(x, statistic = "Both"){
-   first_digit <- NULL
+   first_digit <- NULL # Create an empty object to store the first digit of each number
+   # Extract the first digit of each number and save them in the vector first_digit
    for(j in 1:length(x)){
       first_digit[j]  <-as.numeric(unlist(strsplit(as.character(x),"")[[j]][1]))
    }
-   distribution <- NULL
-   equation_term <- NULL
+   distribution <- NULL # Generate an empty object to store the number distribution
+   equation_term <- NULL # Generate an empty object to store the main component of the formulas
+   # Compute the distribution of numbers and the equation term.
    for(j in unique(first_digit)){
       distribution[j] <- sum(first_digit==j)/length(first_digit) 
       equation_term[j] <- (distribution[j]-log10(1 + 1/ j))
    }
-   names(distribution)<-sort(unique(first_digit))
+   names(distribution)<-sort(unique(first_digit)) # Assign a name to the vector of distribution.
    
+   # Compute both statistics and return them, if statistic == "Both"
    if (statistic == "Both"){
       m <- max(equation_term)
       d <- sqrt(sum((equation_term)^2))
       return(list(m = m, d = d, distribution = distribution))
    }
+   # Compute Leemis' m statistic and return it, if  statistic == "m"
    if (statistic == "m"){
       m <- max(equation_term)
       return(list(m = m, distribution = distribution))
    }
+   # Compute Cho-Gain's d statistic and return it, if statistic == "d"   
    if (statistic == "d"){
       d <- sqrt(sum((equation_term)^2))
       return(list(d = d, distribution = distribution))
@@ -52,13 +57,14 @@ benford <- function(x, statistic = "Both"){
 # Examples:
 benford(data_matrix)
 benford(data_vector, statistic = "m")
+benford(data_vector, statistic = "d")
 
 # Generates print.benfords()
-
-# 
-
 print.benfords <- function(x){
-   if(!is.null(x[["m"]])){
+   if (!is.list(x)){
+      stop("Please, use an object contains the outcome of benford.")
+   }
+   if (!is.null(x[["m"]])){
       dfm <- data.frame(Estimate = x[["m"]], 
                         Stars = c(""), 
                         row.names = c("Leemis' m"),
@@ -71,7 +77,7 @@ print.benfords <- function(x){
          dfm$Stars[1] <- "***"
       }
    }
-   if(!is.null(x[["d"]])){
+   if (!is.null(x[["d"]])){
       dfd <- data.frame(Estimate = x[["d"]], 
                         Stars = c(""), 
                         row.names = c("Cho-Gain's d"),
@@ -84,7 +90,7 @@ print.benfords <- function(x){
          dfd$Stars[1] <- "***"
       }
    }
-   if(exists("dfm") & exists("dfd")){
+   if (exists("dfm") & exists("dfd")){
       df <- rbind(dfm, dfd)
    } else if (exists("dfm") & !exists("dfd")){
       df <- dfm
@@ -97,9 +103,19 @@ print.benfords <- function(x){
    print(outcome)
 }
 
-
-
 # Examples:
+print.benfords(seq(1,10)) 
 print.benfords(benford(data_matrix))  
 print.benfords(benford(data_matrix, statistic = "m"))
 print.benfords(benford(data_matrix, statistic = "d"))
+
+# Generate save_benford_csv      
+save_benfords_csv <- function(x, path = NULL, file.name = "benford.table"){
+   outputpath <- paste0(path, file.name,".csv")
+   sink(outputpath)
+   print.benfords(x)
+   sink()
+}
+
+# Example:
+save_benfords_csv(benford(data_matrix), path = "~/Desktop/", file.name = "test")
